61f35c688d0a5e9619d586c53635a819af206d01
kendall-budget-lab <joshua.m.kendall@yale.edu>
2025-09-25 09:54:23 -0400

    last touches

--- src/main.py
+++ src/main.py
@@ -11,9 +11,8 @@ from calc_edu_dd import build_mis_edu
 from calc_ind_dd import build_mis_ind
 
 stamp = datetime.datetime.now().strftime('%Y%m%d%H%M')
-stamp = "202509250907"
-#out_path = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/AI-Employment-Model", stamp)
-out_path = os.path.join("/gpfs/gibbs/project/sarin/jmk263/Repositories/AI-Employment-Model/output", stamp)
+out_path = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/AI-Employment-Model", stamp)
+#out_path = os.path.join("/gpfs/gibbs/project/sarin/jmk263/Repositories/AI-Employment-Model/output", stamp)
 
 
 if not os.path.exists(out_path):
@@ -54,78 +53,77 @@ ID = '2025092202_20-25'
 start_period = "2021.12"
 end_period   = "2025.8"
 
-# # Build dissimilarity data by historical time periods
-# params = pandas.DataFrame({
-#     'ID':    ['2025092312', '2025092313', '2025092202_14-19', '2025092202_20-25'],
-#     'start': ["1983.2","1995.2", "2015.2", start_period],
-#     'end':   ["1989.12", "2002.12", "2019.12", end_period],
-#     'label': ["Computers", "Internet", "Control", "AI"]
-#     }
-# )
+# Build dissimilarity data by historical time periods
+params = pandas.DataFrame({
+    'ID':    ['2025092312', '2025092313', '2025092202_14-19', '2025092202_20-25'],
+    'start': ["1983.2","1995.2", "2015.2", start_period],
+    'end':   ["1989.12", "2002.12", "2019.12", end_period],
+    'label': ["Computers", "Internet", "Control", "AI"]
+    }
+)
 
-# deltas = pandas.DataFrame(columns=['MONTH', "dissimilarity"])
-# for i in range(0, len(params)):
-#     deltas = pandas.concat([deltas, build_mismatch(params.loc[i, 'ID'], params.loc[i, 'start'], params.loc[i, 'end'], freq=12, pre_trend=12).assign(
-#         label = params.loc[i, 'label']
-#     )])
+deltas = pandas.DataFrame(columns=['MONTH', "dissimilarity"])
+for i in range(0, len(params)):
+    deltas = pandas.concat([deltas, build_mismatch(params.loc[i, 'ID'], params.loc[i, 'start'], params.loc[i, 'end'], freq=12, pre_trend=12).assign(
+        label = params.loc[i, 'label']
+    )])
 
-# deltas.pivot(
-#     index   = 'months_gone',
-#     columns = 'label',
-#     values  = 'dissimilarity'
-# ).reset_index().to_csv(os.path.join(out_path, "dissimilarity.csv"), index = False)
-
-
-# # Build dissimilarity data by recent time periods
-# params = pandas.DataFrame({
-#     'ID':    [ID, ID, ID, ID],
-#     'start': ["2020.02","2021.02", "2021.12", "2021.08"],
-#     'end':   [end_period, end_period, end_period, end_period],
-#     'label': ["Jan21", "Jan22", "AI (Nov22)", "Jul22"]
-#     }
-# )
+deltas.pivot(
+    index   = 'months_gone',
+    columns = 'label',
+    values  = 'dissimilarity'
+).reset_index().to_csv(os.path.join(out_path, "dissimilarity.csv"), index = False)
 
 
-# # By industry analyses
-# recent = pandas.DataFrame({
-#     'name':['recent', 'information', 'financial_activities', 'professional_and_business_services'],
-#     'industry': ['','6470.6860','6870.7260','7270.7790']
-# })
-
-# for j in range(0, len(recent)):
-#     print(recent.loc[j,'industry'])
-#     deltas = pandas.DataFrame(columns=['MONTH', "dissimilarity"])
-#     for i in range(0, len(params)):
-#         deltas = pandas.concat([deltas, build_mismatch(params.loc[i, 'ID'], params.loc[i, 'start'], params.loc[i, 'end'], 12, '', recent.loc[j,'industry']).assign(
-#             label = params.loc[i, 'label']
-#         )])
-#     name = recent.loc[j, 'name'] + ".csv"
-#     deltas.pivot(
-#         index   = 'months_gone',
-#         columns = 'label',
-#         values  = 'dissimilarity'
-#     ).reset_index().to_csv(os.path.join(out_path, name), index = False)
-
-# params2 = pandas.DataFrame({
-#     'label':['all', 'information', 'financial_activities', 'professional_and_business_services'],
-#     'industry': ['','6470.6860','6870.7260','7270.7790']
-# })
-# deltas = pandas.DataFrame(columns=['MONTH', "dissimilarity"])
-# for i in range(0, len(params2)):
-#         deltas = pandas.concat([deltas, build_mismatch(ID, start_period, end_period, 12, '', params2.loc[i,'industry']).assign(
-#             label = params2.loc[i, 'label']
-#         )])
-# deltas.pivot(
-#     index   = 'months_gone',
-#     columns = 'label',
-#     values  = 'dissimilarity'
-# ).reset_index().to_csv(os.path.join(out_path, 'industry.csv'), index = False)
+# Build dissimilarity data by recent time periods
+params = pandas.DataFrame({
+    'ID':    [ID, ID, ID, ID],
+    'start': ["2020.02","2021.02", "2021.12", "2021.08"],
+    'end':   [end_period, end_period, end_period, end_period],
+    'label': ["Jan21", "Jan22", "AI (Nov22)", "Jul22"]
+    }
+)
 
 
-# # By recency of graduation
-# recent_grads = build_mis_edu(ID, "2020.11", end_period, 3).to_csv(os.path.join(out_path, "recent_grads_dissimilarity.csv"), index = False)
+# By industry analyses
+recent = pandas.DataFrame({
+    'name':['recent', 'information', 'financial_activities', 'professional_and_business_services'],
+    'industry': ['','6470.6860','6870.7260','7270.7790']
+})
+
+for j in range(0, len(recent)):
+    print(recent.loc[j,'industry'])
+    deltas = pandas.DataFrame(columns=['MONTH', "dissimilarity"])
+    for i in range(0, len(params)):
+        deltas = pandas.concat([deltas, build_mismatch(params.loc[i, 'ID'], params.loc[i, 'start'], params.loc[i, 'end'], 12, '', recent.loc[j,'industry']).assign(
+            label = params.loc[i, 'label']
+        )])
+    name = recent.loc[j, 'name'] + ".csv"
+    deltas.pivot(
+        index   = 'months_gone',
+        columns = 'label',
+        values  = 'dissimilarity'
+    ).reset_index().to_csv(os.path.join(out_path, name), index = False)
+
+params2 = pandas.DataFrame({
+    'label':['all', 'information', 'financial_activities', 'professional_and_business_services'],
+    'industry': ['','6470.6860','6870.7260','7270.7790']
+})
+deltas = pandas.DataFrame(columns=['MONTH', "dissimilarity"])
+for i in range(0, len(params2)):
+        deltas = pandas.concat([deltas, build_mismatch(ID, start_period, end_period, 12, '', params2.loc[i,'industry']).assign(
+            label = params2.loc[i, 'label']
+        )])
+deltas.pivot(
+    index   = 'months_gone',
+    columns = 'label',
+    values  = 'dissimilarity'
+).reset_index().to_csv(os.path.join(out_path, 'industry.csv'), index = False)
+
+
+# By recency of graduation
+recent_grads = build_mis_edu(ID, "2020.11", end_period, 3).to_csv(os.path.join(out_path, "recent_grads_dissimilarity.csv"), index = False)
 recent_grads = build_mis_edu("2025092202", "2014.11", end_period, 3).to_csv(os.path.join(out_path, "recent_grads_dissimilarity2.csv"), index = False)
-
 
 
 # Builds exposure/usage metrics over time
